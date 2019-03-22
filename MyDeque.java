1d05b7463ebcabe1fe6f1c0935145726e5be178a
moodyRahman <mrahman01@stuy.edu>
2019-03-22 14:40:16 -0400

    k code

--- MyDeque.java
+++ MyDeque.java
@@ -1,7 +1,43 @@
-public class MyDeque{
-	
-public MyDeque(){
-	
+public class MyDeque<E>{
+
+private E[] data;
+private int size, start, end;
+
+MyDeque(){
+
+}
+MyDeque(int initialCapacity){
+
+}
+int size(){
+
+}
+String toString(){
+
+}
+
+void addFirst(E element){
+
+}
+
+void addLast(E element){
+
+}
+
+E removeFirst(){
+
+}
+
+E removeLast(){
+
+}
+
+E getFirst(){
+
+}
+
+E getLast(){
+
 }
 
 }
